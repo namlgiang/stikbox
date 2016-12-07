7c133ef7fad4c7f615b41ab89cde2d7332d3dfbc
Giang Nam <namle@kinssolutions.net>
2016-12-07 16:17:13 +0700

    asd

--- public/script.js
+++ public/script.js
@@ -3,33 +3,15 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-44699912-27', 'auto');
+ga('create', 'UA-44699912-28', 'auto');
 ga('send', 'pageview');
 
 $(document).ready(function() {
-  $(".color li").click(function() {
-    $(".color li").removeClass("active");
-    $(this).addClass("active");
-    var id=$(this).index(".color li");
-    $(".product").css("background-image", "url(product/"+id+".jpg)");
-    $(".form").removeClass("active");
-    $(".form-" + id).addClass("active");
-    $(".purchase>h2").text($(".form-" + id).attr("pname"));
-  });
 
   $(".buy").click(function() {
     $('html, body').animate({
       scrollTop: $(".product").offset().top - 200
     }, 1000);
-  });
-
-  $(".tab>div").click(function() {
-    $(".tab>div").removeClass("active");
-
-    $(".tab-content>div").removeClass("active");
-    $(".tab-content ." + $(this).attr("class")).addClass("active");
-
-    $(this).addClass("active");
   });
 
   $("form").submit(function() {
